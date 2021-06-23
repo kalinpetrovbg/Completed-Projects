f3a4e072ab6f3de731f1d0aeb3fed0285feb8f1d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-23 10:55:20 +0300

    Update settings.py
    remove secret key

--- Django - Beaches to Visit/second_todo/settings.py
+++ Django - Beaches to Visit/second_todo/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_9i_!#_w=&5&3xf8kd##cqu+f&9urbvp8cv8c!^5u380&_a)dt'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
